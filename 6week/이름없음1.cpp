95826ee0c540bfbb0a4edfb4c9b5a40736b9bc75
Trigger0011 <s25048@heungdeok.ms.kr>
2025-07-12 14:47:53 +0900

    Add files via upload

---
+++ 6week/이름없음1.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main( )
+{
+	int a, b;
+	
+	
+	a = 20;
+	b = 5;
+	
+	
+	
+	
+	a += b;
+	printf("a = %d\n", a);
+	a *= b;
+	printf("a = %d\n", a);
+	a -= b;
+	printf("a = %d\n", a);
+	a /= b;
+	printf("a = %d\n", a);
+	a %= b;
+	printf("a = %d\n", a);
+	
+	
+	
+	
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ 6week/재밋는거.cpp
@@ -0,0 +1,97 @@
+#include <stdio.h>
+
+
+int main()
+{
+	int n1;
+	
+	 
+	printf("11X11=");
+	scanf("%d",&n1);
+	
+	if(n1 == 121)
+	{
+		printf("정답!\n");
+	}
+	
+	
+	else
+	{
+		printf("오답!\n");
+	}
+	
+	
+	int n2;
+	
+	 
+	printf("\n \n \n \n100/25=");
+	scanf("%d",&n2);
+	
+	if(n2 == 4)
+	{
+		printf("정답!\n");
+	}
+	
+	
+	else
+	{
+		printf("오답!\n");
+	}
+	
+	
+	int n3 ;
+	
+	 
+	printf("\n \n \n \n 9999/11=");
+	scanf("%d",&n3);
+	
+	if(n3 == 909)
+	{
+		printf("정답!\n");
+	}
+	
+	
+	else
+	{
+		printf("오답!\n");
+	}
+	
+	
+	int n4 ;
+	
+	 
+	printf("\n \n \n \n 10X10X10X10X1.2=");
+	scanf("%d",&n4);
+	
+	if(n4 == 12000)
+	{
+		printf("정답!\n");
+	}
+	
+	
+	else
+	{
+		printf("오답!\n");
+	}
+	
+	
+int n5 ;
+	
+	 
+	printf("\n \n \n \n 917X719=");
+	scanf("%d",&n5);
+	
+	if(n5 == 659323)
+	{
+		printf("정답!\n");
+	}
+	
+	
+	else
+	{
+		printf("오답!\n");
+	}
+	
+	
+	printf("\n \n \n \n 끝!");
+}
